da3d573afc17f45d63bde6322fe2cb3fb7168f20
Salvati-glitch <salvatiopp@gmail.com>
2024-12-06 17:18:35 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+let homeScoreBtnOne = document.getElementById("home-score-btn-1")
+let homeStoreEl =document.getElementById("home-score")
+let homeScore = 0
+
+function increaseHomeScoreOne(){
+    homeScore += 1
+    homeStoreEl.textContent = homeScore
+}
+
+let homeScoreBtnTwo = document.getElementById("home-score-btn-2")
+let homeStoreEl1 =document.getElementById("home-score")
+
+
+function increaseHomeScoreTwo(){
+    homeScore += 2
+    homeStoreEl1.textContent = homeScore
+}
+
+let homeScoreBtnThree = document.getElementById("home-score-btn-3")
+let homeStoreEl2 =document.getElementById("home-score")
+
+
+function increaseHomeScoreThree(){
+    homeScore += 3
+    homeStoreEl2.textContent = homeScore
+}
+
+
+let guestScoreBtnOne = document.getElementById("guest-score-btn-1")
+let guestStoreEl =document.getElementById("guest-score")
+let guestScore = 0
+
+function increaseGuestScoreOne(){
+    guestScore += 1
+    guestStoreEl.textContent = guestScore
+}
+
+let guestScoreBtnTwo = document.getElementById("guest-score-btn-2")
+let guestStoreEl1 =document.getElementById("guest-score")
+
+
+function increaseGuestScoreTwo(){
+    guestScore += 2
+    guestStoreEl1.textContent = guestScore
+}
+
+let guestScoreBtnThree = document.getElementById("guest-score-btn-3")
+let guestStoreEl2 =document.getElementById("guest-score")
+
+
+function increaseGuestScoreThree(){
+    guestScore += 3
+    guestStoreEl2.textContent = guestScore
+}
+
+
+
+
+
+
+
